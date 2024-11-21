babab3890c79c100a9b83052d35c8830466c07b2
guilhermeFlorencio <guilhermedev.florencio@outlook.com>
2024-11-21 10:44:59 -0300

    Removendo web.php

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WeatherController;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/api/weather', [WeatherController::class, 'getWeather']);
